5d039e83b7d6dba5e54254d15818f20bc857d4b0
FooQoo <horseshoes.buzz.17@gmail.com>
2020-12-31 22:26:22 +0900

    コメント追加

--- src/main/java/com/fooqoo56/kyogofinder/api/application/exception/ValidationException.java
+++ src/main/java/com/fooqoo56/kyogofinder/api/application/exception/ValidationException.java
@@ -4,18 +4,38 @@ public class ValidationException extends RuntimeException {
 
     private static final long serialVersionUID = -3147065045035775676L;
 
+    /**
+     * デフォルトコンストラクタ
+     */
     public ValidationException() {
 
     }
 
+    /**
+     * コンストラクタ
+     *
+     * @param message メッセージ
+     */
     public ValidationException(final String message) {
         super(message);
     }
 
+    /**
+     * コンストラクタ
+     *
+     * @param message メッセージ
+     * @param cause   Throwable
+     */
     public ValidationException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * コンストラクタ
+     *
+     * @param errorType エラー種別
+     * @param msg       メッセージ
+     */
     public ValidationException(final String errorType, final String msg) {
         super(errorType + " : " + msg);
     }
